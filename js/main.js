c975ce8ecb0546e986835c33b30ae8f1fc9890a8
DOKIIIIII <lzs1991@163.com>
2015-12-28 18:47:31 +0800

    2

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+
+  'use strict';
+
+  var Cropper = window.Cropper;
+  var console = window.console || { log: function () {} };
+  var container = document.querySelector('.img-container');
+  var image = container.getElementsByTagName('img').item(0);
+  var download = document.getElementById('download');
+  var actions = document.getElementById('actions');
+  var dataX = document.getElementById('dataX');
+  var dataY = document.getElementById('dataY');
+  var dataHeight = document.getElementById('dataHeight');
+  var dataWidth = document.getElementById('dataWidth');
+  var dataRotate = document.getElementById('dataRotate');
+  var dataScaleX = document.getElementById('dataScaleX');
+  var dataScaleY = document.getElementById('dataScaleY');
+  var options = {
+      };
+  var cropper = new Cropper(image, options);
+
+  // Import image
+  var inputImage = document.getElementById('inputImage');
+  var URL = window.URL || window.webkitURL;
+  var blobURL;
+
+  if (URL) {
+    inputImage.onchange = function () {
+      var files = this.files;
+      var file;
+
+      if (cropper && files && files.length) {
+        file = files[0];
+
+        if (/^image\/\w+/.test(file.type)) {
+          blobURL = URL.createObjectURL(file);
+          cropper.reset().replace(blobURL);
+          inputImage.value = null;
+        } else {
+          window.alert('Please choose an image file.');
+        }
+      }
+    };
+  } else {
+    inputImage.disabled = true;
+    inputImage.parentNode.className += ' disabled';
+  }
+
+};
